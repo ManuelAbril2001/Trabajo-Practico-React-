dd0c4c59bfdcfe1c41ce6e004e579e4f7e3c0bc1
valencampi <vcampitelli@udesa.edu.ar>
2021-04-20 14:24:15 -0300

    agregra, borrar, detalle casi

--- tp-react/src/components/Tarjeta.js
+++ tp-react/src/components/Tarjeta.js
@@ -13,7 +13,7 @@ class Tarjeta extends Component{
       }
     
       componentDidMount() {
-        fetch("https://randomuser.me/api/?inc=name,login,email,picture,dob&results=10")
+        fetch("https://randomuser.me/api/?results=2")
           .then(res => res.json())
           .then(
             (data) => {
@@ -32,6 +32,24 @@ class Tarjeta extends Component{
           )
       }
 
+  agregarTarjeta(){
+      fetch("https://randomuser.me/api/")
+       .then(resource => resource.json())
+       .then(data => {
+           console.log(data)
+           this.state.items.push(data.results[0]);
+           this.setState({items: this.state.items});
+       })
+   }
+
+   borrarTarjeta(idItem){
+     console.log(idItem)
+      let resultado = this.state.items.filter((item)=> {
+          return item.login.uuid!== idItem
+      })
+      this.setState({items: resultado});
+  }
+
   render() {
     const { error, isLoaded, items } = this.state;
     if (error) {
@@ -40,31 +58,46 @@ class Tarjeta extends Component{
       return <div>Loading...</div>;
     } else {
       return (
+        <React.Fragment>
+        <h1 className="titulo">T A R J E T A S</h1>
         <ul>
+          <div><button className="agregar" onClick={this.agregarTarjeta.bind(this)}> Agregar X tarjetas </button></div>
+          <br></br>
           {items.map(item => (
             <div className="info" key={item.login.uuid}>
               Nombre y apellido: {item.name.first} {item.name.last} <br></br>
               <img src={item.picture.medium} className="imagen"/>
               <div> Email: {item.email} </div>
               <div> Fecha de nacimiento: {item.dob.date} ({item.dob.age}) </div>
+              <div>
+                <button className="detalle"> Ver más detalles </button> 
+                  <div className="masInfo" id="MasInfo"> 
+                    <div>Calle y número:{item.location.street.number} </div>
+                    <div>Ciudad: {item.location.city} </div>
+                    <div>País: {item.location.country}</div>
+                    <div>Código postal: {item.location.postcode}</div>
+                    <div>Fecha de registro: {item.registered.date}</div>
+                    <div>Teléfono: {item.phone}</div>
+                  </div>
+              </div>
+              <br></br>
+              <button className="borrar" onClick={this.borrarTarjeta.bind(this, item.login.uuid)}>
+                    Eliminar esta tarjeta
+              </button>
             </div>
           ))}
         </ul>
-      );
+        <div>
+              <h4>Filtrar tarjetas </h4>
+              <h6>Nombre</h6>
+              <h6>Apellido</h6>
+              <h6>Edad</h6>
+              <h6>Ordenar de forma ascendente/descendente</h6>
+            </div>
+        </React.Fragment>
+      )
     }
   }
 }
-
-
-    // render() {return( 
-        // <React.Fragment>
-         //    <h1 className="titulo"> Tarjetas </h1>
-          //  <p> <Info apellido= "Campitelli" nombre="Valentina" email="valentinacampitelli@gmail.com" nacimiento= "14/10/2000 (20)"/> </p>
-          //  <p> <Info apellido="Abril" nombre="Manuel" email="" nacimiento= "14/10/2000 (20)"/> </p>
-         //   <p> <Info apellido="Capparelli" nombre="Matias" email="" nacimiento= "14/10/2000 (20)"/> </p>
-      //  </React.Fragment>
-  //  )
-  //  }
-//}
 
 export default Tarjeta;
